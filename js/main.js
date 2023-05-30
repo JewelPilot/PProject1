ed6e3ee99a6068c9d352a961b754ae6fe633e887
JewelPilot <nwagwu.chinmeri@gmail.com>
2023-05-30 11:45:58 -0500

    Basic CSS theme, along with Javascript for button implementation added.

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// Back to top button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  let topButton = document.getElementById("topBtn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// Smooth scrolling for internal links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
